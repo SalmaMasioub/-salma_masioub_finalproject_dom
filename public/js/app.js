9fc357e12ae1fefbfb4d5c7dcb544968a24b6c2e
SalmaMasioub <salma.masioub6@gmail.com>
2025-05-08 21:48:47 +0100

    fixingnavbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,32 @@
-let hello = document.getElementById("h")
+const buttons = {
+  all: document.getElementById("all"),
+  starters: document.getElementById("starters"),
+  salads: document.getElementById("salads"),
+  specialty: document.getElementById("specialty")
+}
+const menuitems = document.querySelectorAll(".menuu .col-12")
 
-hello.addEventListener("click" , ()=>{
-    alert("hello")
+function filterMenu(category) {
+  menuitems.forEach(item => {
+    const shouldShow = category === "all" || item.classList.contains(category)
+    item.classList.toggle("hidden", !shouldShow)
+    if (shouldShow) {
+      item.style.display = "block"
+    }
+  });
+}
+
+
+Object.entries(buttons).forEach(([category, btn]) => {
+  btn.addEventListener("click", () => {
+    filterMenu(category)
+    setActiveButton(btn)
+  })
 })
+
+
+filterMenu("all")
+setActiveButton(buttons.all)
+
+
+
